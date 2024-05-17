5bf4634ba983a6845242323a69c19ede1c3a7973
admin <admin@shenzilong.cn>
2024-05-17 09:56:30 +0000

    fix workspace

--- bash_boot.sh
+++ bash_boot.sh
@@ -1,2 +1,3 @@
+mkdir ./siyuan_workspace
 export siyuanArg="-wd ./resources/ -workspace ./siyuan_workspace/ -accessAuthCode test -alsologtostderr ./err.log -lang zh_CN -log_dir ./siyuan_workspace/"
 bash ./bootstrap.sh
